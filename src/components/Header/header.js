53402a60752334e9fae247d626a9183d319735d0
Daniel Barenboim <barendan@gmail.com>
2019-03-12 18:16:36 -0400

    started building header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,8 +5,14 @@ import './header.css';
 class header extends Component {
   render() {
     return (
-      <div>
-      	Show Header
+      <div className="top_header">
+      	<div className="greeting">
+      		Welcome back Daniel Barenboim
+      	</div>
+      	<div className="menu">
+      		<img alt="self" src=""/>
+      		Menu
+      	</div>
       </div>
     );
   }
