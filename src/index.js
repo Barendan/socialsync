fed74b629966b895b3cc154ecf9cc8e514449139
Daniel Barenboim <barendan@gmail.com>
2019-04-05 12:53:50 -0400

    wiring the html together

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class App extends Component {
   }
 
   componentDidMount(){
-    this.myTween = TweenLite.to(this.myElement, 1, {x:100, y:100});
+    this.myTween = TweenLite.to(this.myElement, 1, {x:300, y:300});
   }
 
   // componentWillMount = () => {
@@ -34,29 +34,133 @@ class App extends Component {
 
 
   renderDisplay = () => (
-    <div>
-      <div className="page one">
+    <div className="app">
+
+
+      <section className="page one">
         <Header/>
 
-        <div className="wrapper">
-          <div ref={div => this.myElement = div}>
-            Hello Hello friend. Whats up
-          </div>
+        <div ref={div => this.myElement = div}>
+          Hello Hello friend. Whats up
         </div>
-      </div>
 
-      <div className="page two">
+      </section>
 
-      </div>
+      <section className="page two">
+        
+        <div className="background">
 
-      <div className="page three">
+          <div className="cover-image">
+            <div className="container container-2">
+            </div>
 
-      </div>
+            <div className="container container-4">
+              <div className="background background-4">
+              </div>
+            </div>
+
+            <div className="container container-6">
+              <div className="background background-6">
+              </div>
+            </div>
+          </div>
+
+          <div className="cover-image">
+
+            <div className="container container-1">
+              <div className="background background-1">
+              </div>
+            </div>
+
+            <div className="container container-3">
+              <div className="background background-3">
+              </div>
+            </div>
+
+            <div className="container container-5">
+              <div className="background background-5">
+              </div>
+            </div>
+          </div>
+
+          <div className="cover-text">
+
+            <div className="container container-1">
+              <div className="title">
+                <div className="text-underline">
+                  
+                  <div className="content">
+                    <p> Skills Make the Man</p>
+                  </div>
+                  <div className="underline">
+                    <div class="background-underline">
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="description">
+                <p> Agora, você pode imprimir por comando de voz, via mobile e desktop. Você dita, a impressora HP imprime em tempo real. Unindo a HP Ink Advantage Ultra com uma API de reconhecimento de voz, criamos uma ferramenta capaz de imprimir historias contadas, instantaneamente. Durante o projeto, configuramos a ferramenta para que os relatos fossem impressos na diagramação exata das páginas do livro. Nossos autores mais urbanos contaram suas histórias de vida usando o APP Magic Words no celular, enquanto outros usaram um microfone comum e a ferramenta no desktop.
+                </p>  
+              </div>
+            </div>
+
+            <div className="container container-2">
+              <div className="title">
+                <div className="text-underline">
+                  
+                  <div className="content">
+                    <p> Skills Make the Man</p>
+                  </div>
+                  <div className="underline">
+                    <div class="background-underline">
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="description">
+                <p> Agora, você pode imprimir por comando de voz, via mobile e desktop. Você dita, a impressora HP imprime em tempo real. Unindo a HP Ink Advantage Ultra com uma API de reconhecimento de voz, criamos uma ferramenta capaz de imprimir historias contadas, instantaneamente. Durante o projeto, configuramos a ferramenta para que os relatos fossem impressos na diagramação exata das páginas do livro. Nossos autores mais urbanos contaram suas histórias de vida usando o APP Magic Words no celular, enquanto outros usaram um microfone comum e a ferramenta no desktop.
+                </p>  
+              </div>
+            </div>
+
+            <div className="container container-3">
+              <div className="title">
+                <div className="text-underline">
+                  
+                  <div className="content">
+                    <p> Skills Make the Man</p>
+                  </div>
+                  <div className="underline">
+                    <div class="background-underline">
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="description">
+                <p> Agora, você pode imprimir por comando de voz, via mobile e desktop. Você dita, a impressora HP imprime em tempo real. Unindo a HP Ink Advantage Ultra com uma API de reconhecimento de voz, criamos uma ferramenta capaz de imprimir historias contadas, instantaneamente. Durante o projeto, configuramos a ferramenta para que os relatos fossem impressos na diagramação exata das páginas do livro. Nossos autores mais urbanos contaram suas histórias de vida usando o APP Magic Words no celular, enquanto outros usaram um microfone comum e a ferramenta no desktop.
+                </p>  
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      
+      </section>
+
+      <section className="page three">
+      </section>
 
     </div>
   )
 
   render() {
+
     return (
       <div>
         {this.renderDisplay()}
