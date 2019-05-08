b7fa344aaa67f1247cd9ac17a74c1e9fb972dae8
Daniel Barenboim <barendan@gmail.com>
2019-05-08 15:14:08 -0400

    finished with validation

--- src/components/Forms/user.js
+++ src/components/Forms/user.js
@@ -16,7 +16,14 @@ class User extends Component {
 					name:'name_input',
 					type:'text',
 					placeholder:'Enter your name'
-				}
+				},
+				validation:{
+					required:true,
+					minLen:5
+				},
+				valid:false,
+				touched:false,
+				validationMessage:''
 			},
 			lastname:{
 				element:'input',
@@ -27,7 +34,13 @@ class User extends Component {
 					name:'lastname_input',
 					type:'text',
 					placeholder:'Enter your lastname'
-				}
+				},
+				validation:{
+					required:true
+				},
+				valid:false,
+				touched:false,
+				validationMessage:''
 			},
 			message:{
 				element:'textarea',
@@ -38,7 +51,11 @@ class User extends Component {
 					name:'message_input',
 					rows:4,
 					cols:36
-				}
+				},
+				validation:{
+					required:false
+				},
+				valid:true
 			},
 			age:{
 				element:'select',
@@ -52,7 +69,11 @@ class User extends Component {
 						{val:'2',text:"20-30"},
 						{val:'3',text:"+30"}
 					]
-				}
+				},
+				validation:{
+					required:false
+				},
+				valid:true
 			}
 		}
 	}
@@ -66,20 +87,30 @@ class User extends Component {
 	submitForm = (event) => {
 		event.preventDefault();
 		let dataToSubmit = {};
+		let formIsValid = true;
 
 		for (let key in this.state.formData){
 			dataToSubmit[key] = this.state.formData[key].value
 		}
 
-		console.log(dataToSubmit);
+		for(let key in this.state.formData){
+			formIsValid = this.state.formData[key].valid && formIsValid;
+		}
+
+		if(formIsValid){
+			console.log(dataToSubmit)
+		}
+
 	}
 
 	render(){
 		return(
 			<div className="container">
 				<form onSubmit={this.submitForm}>
+				
 					<FormFields
 						formData={this.state.formData}
+						onblur={ (newState) => this.updateForm(newState)}
 						change={ (newState) => this.updateForm(newState)}
 					/>
 
