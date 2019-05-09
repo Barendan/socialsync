a1bd167e7a980584901c503076ec064cab88808f
Daniel Barenboim <barendan@gmail.com>
2019-05-09 11:39:12 -0400

    cleaned up code

--- src/components/app.js
+++ src/components/app.js
@@ -4,22 +4,26 @@ import './app.css';
 import Header from './Header/header';
 import UserList from '../containers/user-list';
 import UserDetail from '../containers/user-detail';
+import Demo from './Demo/demo';
 
-import User from './Forms/user';
+// import User from './Forms/user';
 
-const App = (props) => {
-
-    return (
-      <div className="page">
-        <Header />
-        <div className="wrapper">
-          <UserList />
-          <User />
-          <UserDetail />
-        </div>
-      </div>
-    )
+const App = (props) => (
+  <div className="page">
     
-}
+    <Header />
+    { props.auth ? 
+      <div className="wrapper">
+        <UserList users={props} />
+        <UserDetail />
+      </div>
+      :
+      <div className="wrapper">
+        <Demo />
+      </div>
+    }
+
+  </div>
+)
 
 export default App;
